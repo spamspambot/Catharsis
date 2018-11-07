e0ac3e6517fbe4ad72dfe9ec1de3c3cc7a299521
spamspambot <spambot@outlook.dk>
2018-11-07 13:02:16 +0100

    Tableflip

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    public Transform mouseTarget;
+    public float offset;
+    Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseTarget.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z + offset));
+        transform.LookAt(mouseTarget);
+    }
+}
