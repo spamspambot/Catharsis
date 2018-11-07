da3e8aba7bd9dad50664e9234b8ec30e4e3588c0
spamspambot <spambot@outlook.dk>
2018-11-07 14:29:50 +0100

    Tableflip + multi tableflip

---
+++ Assets/Scripts/TableCloneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableCloneScript : MonoBehaviour
+{
+    public GameObject realTable;
+    public Vector3 startPos;
+    public Vector3 realTableStartPos;
+    Rigidbody rb;
+    TableScript tableScript;
+    public bool flipped;
+    public float flipMultiplier;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        realTableStartPos = realTable.transform.position;
+        tableScript = realTable.GetComponent<TableScript>();
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tableScript.triggerEnter && !flipped) { rb.velocity = tableScript.throwVelocity * flipMultiplier; flipped = true; }
+
+
+    }
+}
---
+++ Assets/Scripts/TableScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScript : MonoBehaviour {
+    Rigidbody rb;
+    public bool triggerEnter;
+    public Vector3 throwVelocity;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void OnTriggerEnter(Collider other) {
+        throwVelocity = rb.velocity;
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (!triggerEnter)
+        {
+            throwVelocity = rb.velocity;
+            triggerEnter = true;
+        }
+    }
+}
