e0481aa734a671ab87c0adeed023e468757da41d
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2018-11-09 12:25:36 +0100

    pause menu

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    private bool isPaused = false;
+    public GameObject pauseScreen;
+    private int sceneIndex;
+
+	// Use this for initialization
+	void Start () {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("Escape"))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused == true)
+            {
+                Paused();
+            }
+            else if (isPaused == false)
+            {
+                Resume();
+            }
+        }
+	}
+
+    public void Paused()
+    {
+        Time.timeScale = 0f;
+
+        pauseScreen.SetActive(true);
+
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+
+        pauseScreen.SetActive(false);
+    }
+
+    public void Restart(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void NextScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
